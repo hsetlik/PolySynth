8ea9ed3dc61a88d459fff48731082853fdbfdb02
Hayden Setlik <haydensetlik@gmail.com>
2024-06-21 15:32:44 -0600

    voice board rev2

--- PolySynthF411/Core/Inc/MCP44x1.h
+++ PolySynthF411/Core/Inc/MCP44x1.h
@@ -17,8 +17,8 @@ extern "C" {
 
 #include "stm32f4xx_hal.h"
 // for two devices addressed differently:
-#define MCP4X11_1_ADDR 0x5A
-#define MCP4X11_2_ADDR 0x5E
+#define MCP44X1_1_ADDR 0x5A
+#define MCP44X1_2_ADDR 0x5E
 
 // per section 7.4 of the datasheet writing to the pot register works like:
 // - address byte (taken care of by the I2C HAL)
@@ -28,6 +28,11 @@ extern "C" {
 uint8_t getWriteByteFor(uint8_t channel);
 
 void MCP44x1_setLevel(I2C_HandleTypeDef* i2c, uint8_t addr, uint8_t channel, uint8_t value);
+
+
+
+
+
 
 #ifdef __cplusplus
 }
