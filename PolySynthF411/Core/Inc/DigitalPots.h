ac2ff50e29e6e3c551a4e6d98d7867a29014f45e
Hayden Setlik <haydensetlik@gmail.com>
2024-07-25 09:38:26 -0600

    AD5280 driver

--- PolySynthF411/Core/Inc/DigitalPots.h
+++ PolySynthF411/Core/Inc/DigitalPots.h
@@ -16,22 +16,23 @@ extern "C" {
 
 //========================================================================
 // Two MCP4451 quad digital pots handle mixing the oscillators
-#define MCP44X1_1_ADDR 0x5A
-#define MCP44X1_2_ADDR 0x5E
+#define MCP44X1_1_ADDR 0x58
+#define MCP44X1_2_ADDR 0x5C
 
 // per section 7.4 of the datasheet writing to the pot register works like:
 // - address byte (taken care of by the I2C HAL)
 // - command byte (see p. 55 of the datasheet)
 // - data byte
 
-uint8_t MCP44x1_writeByte(uint8_t channel);
-
 void MCP44x1_setLevel(I2C_HandleTypeDef* i2c, uint8_t addr, uint8_t channel, uint8_t value);
 
 
 //========================================================================
-// the filter resonance is controlled by a MCP45HVX1 digital pot
-#define MCP45XX_ADDR 0x78
+// the filter resonance is controlled by a AD5280 digital pot
+#define AD5280_ADDR 0x5A
+
+void AD5280_setLevel(I2C_HandleTypeDef* i2c, uint8_t addr, uint8_t value);
+
 
 
 #ifdef __cplusplus
--- PolySynthF411/Core/Src/DigitalPots.c
+++ PolySynthF411/Core/Src/DigitalPots.c
@@ -36,3 +36,36 @@ void MCP44x1_setLevel(I2C_HandleTypeDef* i2c, uint8_t addr, uint8_t channel, uin
 
 
 //==================================================================
+
+// all this bit stuff is explained on pg 16 of the AD5280 datasheet
+uint8_t AD5280_addressByte(uint8_t addr, uint8_t read) {
+	if(read)
+		return addr | 1;
+	return addr & ~1;
+}
+
+// some helpers for bit logic stuff
+/*
+ * Per the datasheet the bits (MSB to LSB order) for the command byte are:
+ * 1. RDAC: RDAC select bit (should always be low for AD5280)
+ * 2. RS: Midscale reset. Logic high will set the wiper to its midpoint
+ * 3. SD: Shutdown. Logic high shorts the wiper to terminal B
+ * 4. O1: o1 logic output
+ * 5. O2: o2 logic output
+ * 6. Don't care
+ * 7. Don't care
+ * 8. Don't care
+ *
+ * (basically we can just send 0x00 in our case woo)
+ * */
+
+
+void AD5280_setLevel(I2C_HandleTypeDef* i2c, uint8_t addr, uint8_t value) {
+	uint16_t addr16 = ((uint16_t)AD5280_addressByte(addr, 0)) << 8;
+	uint8_t data[2];
+	data[0] = 0x00;
+	data[1] = value;
+	if(HAL_I2C_Master_Transmit(i2c, addr16, data, 2, HAL_MAX_DELAY)!= HAL_OK){
+		// uh oh
+	}
+}
